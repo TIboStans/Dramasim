b46fb53927a4ec420e1a1951166a30732907ae61
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-12-10 21:22:13 +0100

    Signed RAM + indexing + util

--- src/state/ram.rs
+++ src/state/ram.rs
@@ -1,17 +1,52 @@
+use std::ops::{Index, IndexMut};
+
 pub type RAM = RandomAccessMemory;
 
 pub struct RandomAccessMemory {
-    inner: [usize; 10_000]
+    inner: [isize; 10_000]
 }
 
 impl RAM {
-    pub fn get(&self, index: usize) -> usize {
-        self.inner[index]
-    }
-
     pub fn new() -> Self {
         RAM {
             inner: [0; 10_000]
         }
     }
+}
+
+impl Index<usize> for RAM {
+    type Output = isize;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.inner[index]
+    }
+}
+
+impl Index<isize> for RAM {
+    type Output = isize;
+
+    fn index(&self, index: isize) -> &Self::Output {
+        &self.inner[address(index)]
+    }
+}
+
+impl IndexMut<usize> for RAM {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.inner[index]
+    }
+}
+
+impl IndexMut<isize> for RAM {
+    fn index_mut(&mut self, index: isize) -> &mut Self::Output {
+        &mut self.inner[address(index)]
+    }
+}
+
+pub fn address(number: isize) -> usize {
+    let a = number % 10_000;
+    if a < 0 { (a + 10_000) as usize } else { a as usize }
+}
+
+pub fn expand(a: usize) -> isize {
+    if a >= 5_000 { a as isize - 10_000 } else { a as isize }
 }
