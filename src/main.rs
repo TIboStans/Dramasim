6dad456e25fff15f3dbc7293538349fe94577ca9
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-12-10 21:22:49 +0100

    Construct CPU+RAM from main, run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use crate::state::cpu::CPU;
+use crate::state::ram::RAM;
+
+mod state {
+    pub mod cpu;
+    pub mod ram;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU::new();
+    let mut ram = RAM::new();
+    ram[0usize] = 99_11_0_0_0000;
+
+    cpu.run(ram);
 }
